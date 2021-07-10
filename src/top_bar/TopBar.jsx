005b9aa14576a2465feff8af60477bd3bb792a25
Daniel Kaniu <danniu254@gmail.com>
2021-07-10 08:31:22 +0300

    social media

--- src/top_bar/TopBar.jsx
+++ src/top_bar/TopBar.jsx
@@ -1,5 +1,15 @@
 import "./topbar.css";
 
 export default function topBar() {
-  return <div className="top">Dinited Blog</div>;
+  return (
+    <div className="top">
+      <div className="topLeft">
+        <i class="fab fa-facebook"></i>
+        <i class="fab fa-twitter"></i>
+        <i class="fab fa-instagram"></i>
+      </div>
+      <div className="topCenter">Kerea</div>
+      <div className="topRight">of Kerea</div>
+    </div>
+  );
 }
